f39b85e46802650a42fa191e027bd695ac691ebd
DDoty99 <jdoty@centurymgmt.com>
2017-08-18 15:12:02 -0700

    initialpush

---
+++ xlstoxlsx.ps1
@@ -0,0 +1,25 @@
+﻿
+$word = New-Object -ComObject excel.application 
+$word.visible = $false 
+$folderpath = Read-Host 'Path?'
+$folderpath2 = $folderpath + '\*'
+$fileType = "*xls" 
+Get-ChildItem -recurse -path $folderpath2 -include $fileType | 
+foreach-object { 
+$path = ($_.fullname).substring(0,($_.FullName).lastindexOf(".")) 
+$docxpath =($_.fullname).substring(0,($_.FullName).lastindexOf(".")) + ".xlsx" 
+Write-Host "$docxpath"
+
+  if (test-path $docxpath) {Write-Host "Skip $docxpath"
+ }
+else {
+"Converting $path to $fileType ..."
+ $doc = $Word.workbooks.open($_.fullname) 
+$doc.saveas($path, [Microsoft.Office.Interop.Excel.XLFileFormat]::xlWorkbookDefault) 
+$doc.close() 
+  }
+}
+$word.Quit() 
+$word = $null 
+[gc]::collect() 
+[gc]::WaitForPendingFinalizers()
