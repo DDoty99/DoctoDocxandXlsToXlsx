7baaa4bab8794918fe81fe5763e717bb88aa9c34
DDoty99 <jdoty@centurymgmt.com>
2017-10-31 15:12:53 -0700

    Smarter way of interperating file names... Doesn't skip over odd chars

--- doctodocx.ps1
+++ doctodocx.ps1
@@ -4,18 +4,26 @@ $word.visible = $false
 $folderpath = Read-Host 'Path?'
 $folderpath = $folderpath + '\*'
 $fileType = "*doc" 
-Get-ChildItem -recurse -path $folderpath -include $fileType | 
+Get-ChildItem -Force -recurse -path $folderpath -include $fileType -ErrorAction SilentlyContinue | 
 foreach-object { 
 $path = ($_.fullname).substring(0,($_.FullName).lastindexOf(".")) 
 $docxpath =($_.fullname).substring(0,($_.FullName).lastindexOf(".")) + ".docx"
-  if (test-path $docxpath) {Write-Host "Skip $docxpath"
- }
-else {"Converting $path to $fileType ..." 
-$doc = $word.documents.open($_.fullname) 
-$doc.saveas([ref] $path, [ref]$SaveFormat::wdFormatDocumentDefault) 
-$doc.close() 
-    }
+  if (test-path -literal $docxpath) {
+}
+else {
+try {
+   write-output "Converting $path to $fileType ..."  
+    $doc = $word.documents.open($_.fullname) 
+    $doc.saveas([ref] $docxpath, [ref]$SaveFormat::wdFormatDocumentDefault) 
+    $doc.close() 
+
+}
+catch {
+Write-Output "$path couldnt be converted"
 } 
+
+}
+}
 $word.Quit() 
 $word = $null 
 [gc]::collect() 
